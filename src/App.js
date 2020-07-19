bbb47d3221cb9078aa6206785576679faeebd60c
Andre Mendes <andre.mendes@vakt.io>
2020-07-19 12:21:08 +0100

    [master][AndreM] section 2 project configuration

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt me!'),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
     React.createElement(Pet, {
-      name: 'Jason',
-      animal: 'Dog',
-      breed: 'Yorkshire',
+      name: "Jason",
+      animal: "Dog",
+      breed: "Yorkshire",
     }),
     React.createElement(Pet, {
-      name: 'Maui',
-      animal: 'Dog',
-      breed: 'Portuguese water Dog',
+      name: "Maui",
+      animal: "Dog",
+      breed: "Portuguese water Dog",
     }),
     React.createElement(Pet, {
-      name: 'Kiara',
-      animal: 'Cat',
-      breed: 'La Perm',
+      name: "Kiara",
+      animal: "Cat",
+      breed: "La Perm",
     }),
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
